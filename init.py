2a3590e0a1baf531cc73d52e7b39fc1f1a6e7c49
Gabriel Zapodeanu <gzapodea@cisco.com>
2018-01-10 21:07:50 -0800

    work in progress

---
+++ init.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3
+
+
+# developed by Gabi Zapodeanu, TSA, GPO, Cisco Systems
+
+
+# This file contains the Spark Auth, Tropo Key, Google Developer Key
+
+SPARK_URL = 'https://api.ciscospark.com/v1'
+SPARK_AUTH = 'Bearer ' + 'Paste your Spark token here'
+
+
+GOOGLE_API_KEY = 'Paste your Google developer API key here'
+
+
+DNAC_URL = 'https://your dna center'
+DNAC_USER = 'user'
+DNAC_PASS = 'password'
